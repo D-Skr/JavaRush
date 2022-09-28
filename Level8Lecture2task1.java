df65fc3eb8fe7faaf468dda883c02c37e6447f6e
Di <altusj@gmail.com>
2022-09-28 12:47:11 -0400

    fix the typo

--- Level8Lecture2task1.java
+++ Level8Lecture2task1.java
@@ -4,7 +4,7 @@ public class Level8Lecture2task1 {
 
 	// package com.javarush.task.jdk13.task06.task0625;
 	// Fill the matrix 3x3 with keyboard input
-	// count sum of each column and string
+	// count sum of each column and row
 	// print the min sum
 
 	public static int[][] array = new int[3][3];
@@ -20,7 +20,7 @@ public class Level8Lecture2task1 {
 
 		int minSum = Integer.MAX_VALUE;
 
-		// string sum
+		// row sum
 		for (int i = 0; i < 3; i++) {
 			int currentSum = array[0][i] + array[1][i] + array[2][i];
 			if (minSum > currentSum) {
