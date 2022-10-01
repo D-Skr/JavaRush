0cee25d50b5027c35d03c86b537415ac839fde9a
Di <altusj@gmail.com>
2022-09-30 20:22:42 -0400

    return index solution

--- Level10Lectrure4.java
+++ Level10Lectrure4.java
@@ -1,9 +1,15 @@
-
 public class Level10Lectrure4 {
 
+	//package com.javarush.task.pro.task09.task0913;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    String string = "Чтобы стать программистом, нужно писать код. Чтобы писать код, нужно учиться. Чтобы учиться, нужно желание.";
+        String word = "код";
+        int indexFromFirstWord = getIndexFromFirstWord(string, word);
+        int indexFromLastWord = getIndexFromLastWord(string, word);
+        System.out.println("Индекс первого символа первого слова \"" + word + "\" равен - " + indexFromFirstWord);
+        System.out.println("Индекс первого символа последнего слова \"" + word + "\" равен - " + indexFromLastWord);
+    }
 
-	}
 
 }
