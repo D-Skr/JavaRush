584b53d5c43555d3d62efc92564e04be80cb43b8
Di <altusj@gmail.com>
2022-10-01 23:02:11 -0400

    add changPath method

--- Level10Lectrure5task2.java
+++ Level10Lectrure5task2.java
@@ -1,22 +1,17 @@
 public class Level10Lectrure5task2 {
 
 	//package com.javarush.task.pro.task09.task0914;
-	//
+	//update jdk path
 	
 	public static void main(String[] args) {
-	    String string = "Чтобы стать программистом, нужно писать код. Чтобы писать код, нужно учиться. Чтобы учиться, нужно желание.";
-        String word = "код";
-        int indexFromFirstWord = getIndexFromFirstWord(string, word);
-        int indexFromLastWord = getIndexFromLastWord(string, word);
-        System.out.println("Индекс первого символа первого слова \"" + word + "\" равен - " + indexFromFirstWord);
-        System.out.println("Индекс первого символа последнего слова \"" + word + "\" равен - " + indexFromLastWord);
+
     }
 
-    public static int getIndexFromFirstWord(String string, String word) {
-        return string.indexOf(word);
-    }
+    public static String changePath(String path, String jdk) {
+        int firstIndex = path.indexOf("jdk");
+        int lastIndex = path.indexOf("/", firstIndex);
+        String oldJdk = path.substring(firstIndex, lastIndex);
 
-    public static int getIndexFromLastWord(String string, String word) {
-        return string.lastIndexOf(word);
+        return path.replace(oldJdk, jdk);
     }
 }
