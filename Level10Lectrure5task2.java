c172d5801df3e004829b35e01f049254579c29a7
Di <altusj@gmail.com>
2022-10-01 23:11:37 -0400

    add case

--- Level10Lectrure5task2.java
+++ Level10Lectrure5task2.java
@@ -4,12 +4,16 @@ public class Level10Lectrure5task2 {
 	//update jdk path
 	
 	public static void main(String[] args) {
-
+		String path = "/usr/java/jdk1.8/bin/";
+        String jdk13 = "jdk-13";
+        System.out.println(changePath(path, jdk13));
     }
 
     public static String changePath(String path, String jdk) {
         int firstIndex = path.indexOf("jdk");
+        System.out.println(firstIndex);
         int lastIndex = path.indexOf("/", firstIndex);
+        System.out.println(lastIndex);
         String oldJdk = path.substring(firstIndex, lastIndex);
 
         return path.replace(oldJdk, jdk);
