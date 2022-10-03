40b37b1f1e543367a89198fbaca885ada21f247e
Di <altusj@gmail.com>
2022-10-03 15:45:49 -0400

    add cases

--- Level11Lecture2task2.java
+++ Level11Lecture2task2.java
@@ -5,9 +5,9 @@ public class Level11Lecture2task2 {
 	 */
 		public static void main(String[] args) {
 	        int decimalNumber = Integer.MAX_VALUE;
-	        System.out.println("Десятичное число " + decimalNumber + " равно двоичному числу " + toBinary(decimalNumber));
+	        System.out.println("Decimal number " + decimalNumber + " equals binary number " + toBinary(decimalNumber));
 	        String binaryNumber = "1111111111111111111111111111111";
-	        System.out.println("Двоичное число " + binaryNumber + " равно десятичному числу " + toDecimal(binaryNumber));
+	        System.out.println("Двоичное число " + binaryNumber + " equals decimal number " + toDecimal(binaryNumber));
 	    }
 
 	    public static String toBinary(int decimalNumber) {
