1f22de09eb37a8e8d9c05ca5e8fa22f297d567cc
Di <altusj@gmail.com>
2022-10-03 15:43:45 -0400

    add converter to binary

---
+++ Level11Lecture2task2.java
@@ -0,0 +1,9 @@
+public class Level11Lecture2task2 {
+	/**
+	 * Package com.javarush.task.pro.task09.task0906;
+	 * convert decimal number to binary
+	 */
+		public static void main(String[] args) {
+
+	}
+}
