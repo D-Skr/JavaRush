3fbeb3c3a5ff9c5273cc102e9a113d38e495217c
Di <altusj@gmail.com>
2022-10-15 11:26:16 -0400

    add objects to JavaRush task

--- Level13Lecture1.java
+++ Level13Lecture1.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Level13Lecture1 {
 	//package com.javarush.task.pro.task13.task1305;
@@ -7,7 +8,19 @@ public class Level13Lecture1 {
 	//case ignoring
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 ArrayList<String> words = new ArrayList<>();
+	        words.add("Hello world!");
+	        words.add("Bob");
+	        words.add("Elly");
+	        words.add("Sam");
+	        words.add("Bug");
+	        words.add("bug");
+	        words.add("B ug");
+	        words.add("Bill");
+
+	        ArrayList<String> copyWordsFirst = new ArrayList<>(words);
+	        ArrayList<String> copyWordsSecond = new ArrayList<>(words);
+	        ArrayList<String> copyWordsThird = new ArrayList<>(words);
 
 	}
 
