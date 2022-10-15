a6ce00162719cf8bc94bb24259a665455724ec08
Di <altusj@gmail.com>
2022-10-15 11:24:47 -0400

    add description to JavaRush task

--- Level13Lecture1.java
+++ Level13Lecture1.java
@@ -3,6 +3,8 @@ public class Level13Lecture1 {
 	//package com.javarush.task.pro.task13.task1305;
 	//create ArrayLists with Strings
 	//Iterate through collection and remove String "bug"
+	//use 3 different ways to remove it.
+	//case ignoring
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
