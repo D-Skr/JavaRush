8ed25f4b44a4cb06754ac99ad2f332048ab55c48
Di <altusj@gmail.com>
2022-10-18 14:42:20 -0400

    add method to my own Collection implementation

--- Level18Lecture3.java
+++ Level18Lecture3.java
@@ -3,17 +3,24 @@ import java.util.Collections;
 import java.util.Random;
 
 public class Level18Lecture3 {
-	
-	//package com.javarush.task.pro.task13.task1306;
-	//implement your own Collections methods copy, addAll, replaceAll
-	//reverse, rotate, shuffle, sort
-	
-	
+
+	// package com.javarush.task.pro.task13.task1306;
+	// implement your own Collections methods copy, addAll, replaceAll
+	// reverse, rotate, shuffle, sort
+
+	// Collections.copy(destination, source);
+	public static void copy(ArrayList<String> destination, ArrayList<String> source) {
+		if (destination.size() < source.size()) {
+			throw new IndexOutOfBoundsException("Source does not fit in dest");
+		}
+		for (int i = 0; i < source.size(); i++) {
+			destination.set(i, source.get(i));
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
 	}
 
 }
