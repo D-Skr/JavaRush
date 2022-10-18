f03ef2bc8281e896d6f4ee251828361a1e59e3e9
Di <altusj@gmail.com>
2022-10-18 14:41:47 -0400

    add method to my own Collection implementation

---
+++ Level18Lecture3.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Level18Lecture3 {
+	
+	//package com.javarush.task.pro.task13.task1306;
+	//implement your own Collections methods copy, addAll, replaceAll
+	//reverse, rotate, shuffle, sort
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
