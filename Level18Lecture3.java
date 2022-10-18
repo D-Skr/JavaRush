6d77529bf1bea4913e2aede1f0ddb916cfd15159
Di <altusj@gmail.com>
2022-10-18 14:51:05 -0400

    add method to my own Collection implementation

--- Level18Lecture3.java
+++ Level18Lecture3.java
@@ -117,6 +117,26 @@ public class Level18Lecture3 {
          return frequency;
     }
 
+    //Collections.sort(list);
+    //Collections.binarySearch(list, key)
+    public static int binarySearch(ArrayList<Integer> list, Integer key) {
+         int low = 0;
+         int high = list.size() - 1;
+         int mid = (low + high) / 2;
+         int index = -1;
+         while (low <= high) {
+             if (list.get(mid) < key) {
+                 low = mid + 1;
+             } else if (list.get(mid).equals(key)) {
+                 index = mid;
+                 break;
+             } else {
+                 high = mid - 1;
+             }
+             mid = (low + high) / 2;
+         }
+         return index;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
